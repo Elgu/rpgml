ec1b433a21c12c378eb053903036d8195ee6aefd
Gunnar Payer <gunnar.payer@web.de>
2013-11-03 11:23:34 +0100

    Started implementing Node, Input and Output

--- RPGML/Node.h
+++ RPGML/Node.h
@@ -2,6 +2,7 @@
 #define RPGML_Node_h
 
 #include "GarbageCollector.h"
+#include "Map.h"
 
 #include <vector>
 
@@ -16,56 +17,78 @@ class Node;
 class Port : public Collectable
 {
 public:
-  Port( GarbageCollector *gc );
+  Port( GarbageCollector *gc, Node *parent, const String *identifier );
   virtual ~Port( void );
 
+  Node *getParent( void ) const;
+
+  virtual void gc_clear( void );
+  virtual CountPtr< Children > getChildren( void ) const;
+
 private:
-  CountPtr< Node > m_node;
-  CountPtr< String > m_identifier;
+  CountPtr< Node > m_parent;
+  CountPtr< const String > m_identifier;
 };
 
 class Input : public Port
 {
 public:
-  Input( GarbageCollector *gc );
+  Input( GarbageCollector *gc, Node *parent, const String *identifier );
   virtual ~Input( void );
 
   virtual void gc_clear( void );
   virtual CountPtr< Children > getChildren( void ) const;
 
+  void disconnect( void );
+  void connect( Output *output );
+
+  bool isConnected( void ) const;
+
 private:
-  std::vector< CountPtr< Output > > m_outputs;
+  CountPtr< Output > m_output;
 };
 
 class Output : public Port
 {
 public:
-  Output( GarbageCollector *gc );
+  Output( GarbageCollector *gc, Node *parent, const String *identifier );
   virtual ~Output( void );
+
+  void disconnect( void );
+  void disconnect( Input *input );
+
+  void connect( Input *input );
+
+  bool isConnected( void ) const;
 
   virtual void gc_clear( void );
   virtual CountPtr< Children > getChildren( void ) const;
 
 private:
-  CountPtr< Input > m_input;
+  typedef std::vector< CountPtr< Input > > inputs_t;
+  inputs_t m_inputs;
 };
 
 class Param : public Collectable
 {
 public:
-  Param( GarbageCollector *gc );
+  Param( GarbageCollector *gc, Node *parent, const String *identifier );
   virtual ~Param( void );
+
+  bool set( const Value &value ) const;
+  Node *getParent( void ) const;
+  const String *getIdentifier( void ) const;
 
   virtual void gc_clear( void );
   virtual CountPtr< Children > getChildren( void ) const;
 
 private:
-  CountPtr< Node > m_node;
-  CountPtr< String > m_identifier;
+  CountPtr< Node > m_parent;
+  CountPtr< const String > m_identifier;
   std::vector< Value > m_values;
 };
 
-class Node : public Collectable
+class Node : public Map
 {
 public:
   Node( GarbageCollector *gc );
