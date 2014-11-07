4e9780ad03b1224803cee4963c216ba2208becf9
gunnar <gunnar.payer@web.de>
2014-11-07 21:04:17 +0100

    Implmemented Thread class using pthread

--- RPGML/Thread.cpp
+++ RPGML/Thread.cpp
@@ -2,26 +2,15 @@
 
 namespace RPGML {
 
-class Thread::Impl
-{
-public:
-  Impl( void )
-  {}
-
-  ~Impl( void )
-  {}
-
-private:
-};
-
 Thread::Thread( GarbageCollector *_gc )
 : Collectable( _gc )
-, impl( new Impl )
+, m_running( false )
 {}
 
 Thread::~Thread( void )
 {
-  delete impl;
+  cancel();
+  join();
 }
 
 void Thread::gc_clear( void )
@@ -29,6 +18,65 @@ void Thread::gc_clear( void )
 
 void Thread::gc_getChildren( Children &children ) const
 {}
+
+bool Thread::isRunning( void ) const
+{
+  return m_running;
+}
+
+bool Thread::start( void )
+{
+  return start( &dispatch_run, this );
+}
+
+bool Thread::start( void *(*start_routine)(void*), void *arg )
+{
+  if( isRunning() ) return false;
+
+  if( 0 != pthread_create( &m_thread, 0, start_routine, arg ) )
+  {
+    return false;
+  }
+
+  m_running = true;
+  return true;
+}
+
+bool Thread::join( void )
+{
+  if( !isRunning() ) return false;
+  void *ret = 0;
+  if( 0 != pthread_join( m_thread, &ret ) ) return false;
+  m_arg.reset();
+  m_running = false;
+  return true;
+}
+
+bool Thread::cancel( void )
+{
+  if( !isRunning() ) return false;
+  if( 0 != pthread_cancel( m_thread ) ) return false;
+  return true;
+}
+
+bool Thread::start( DispatchArg *arg )
+{
+  return start( &dispatch_arg, arg );
+}
+
+void *Thread::dispatch_arg( void *arg )
+{
+  DispatchArg *const dispatch_arg = (DispatchArg*)arg;
+  dispatch_arg->run();
+  return 0;
+}
+
+void *Thread::dispatch_run( void *arg )
+{
+  Thread *const thread = (Thread*)arg;
+  thread->run();
+  return 0;
+}
 
 } // namespace RPGML
 
--- RPGML/Thread.h
+++ RPGML/Thread.h
@@ -3,6 +3,9 @@
 
 #include "GarbageCollector.h"
 
+#include <memory>
+#include <pthread.h>
+
 namespace RPGML {
 
 class Thread : public Collectable
@@ -16,12 +19,68 @@ public:
   virtual void gc_clear( void );
   virtual void gc_getChildren( Children &children ) const;
 
-protected:
+  bool isRunning( void ) const;
 
+  // starts in run()
+  bool start( void );
+
+  // starts in function, starts with arg
+  bool start( void *(*start_routine)(void*), void *arg );
+
+  // starts in method
+  template< class ParentClass >
+  bool start( ParentClass *parent, void (ParentClass::*method)( void ) )
+  {
+    if( isRunning() ) return false;
+    m_arg.reset( new DispatchMethod< ParentClass >( parent, method ) );
+    return start( m_arg.get() );
+  }
+
+  // Waits for thread to finish, returns whether it was running
+  bool join( void );
+
+  // Cancels thread if running, join() should be called if one wants to wait
+  // for the cancel to have taken effect returns whether it was running
+  bool cancel( void );
+
+protected:
+  virtual void run( void ) {}
 
 private:
-  class Impl;
-  Impl *const impl;
+  struct DispatchArg
+  {
+    DispatchArg( void ) {}
+    virtual ~DispatchArg( void ) {}
+    virtual void run( void ) = 0;
+  };
+
+  template< class ParentClass >
+  struct DispatchMethod : public DispatchArg
+  {
+    DispatchMethod( ParentClass *_parent, void (ParentClass::*_method)( void ) )
+    : parent( _parent )
+    , method( _method )
+    {}
+
+    virtual ~DispatchMethod( void ) {}
+
+    ParentClass *parent;
+    void (ParentClass::*method)( void );
+
+    virtual void run( void )
+    {
+      (parent->*method)();
+    }
+  };
+
+  bool start( DispatchArg *arg );
+
+  static void *dispatch_arg( void *arg );
+  static void *dispatch_run( void *arg );
+
+  std::auto_ptr< DispatchArg > m_arg;
+  pthread_t m_thread;
+  bool m_running;
 };
 
 
