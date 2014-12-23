38b93a34f7ebc78dece09569dede84ba99909b9b
gunnar <gunnar.payer@web.de>
2014-12-23 21:20:00 +0100

    Added licence notices

--- ROOT/RPGML_Node_BinaryOp.cpp
+++ ROOT/RPGML_Node_BinaryOp.cpp
@@ -1,3 +1,20 @@
+/* This file is part of RPGML.
+ * 
+ * Copyright (c) 2014, Gunnar Payer, All rights reserved.
+ * 
+ * RPGML is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
 #include "RPGML_Node_BinaryOp.h"
 
 namespace RPGML {
