c291d409574a4ebeeba3ae2e1e7c0183279d6709
gunnar <gunnar.payer@web.de>
2015-01-06 23:41:29 +0100

    Moved GarbageCollector outside Context is now Collectable Adding "@needed" to root Frame Added search path for plugins and scripts

--- libRPGML/RPGML/Context.cpp
+++ libRPGML/RPGML/Context.cpp
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -22,18 +22,69 @@
 #include "Frame.h"
 #include "StringUnifier.h"
 
+#include <string>
+
 namespace RPGML {
 
-Context::Context( StringUnifier *unifier )
-: m_unifier( unifier )
+Context::Context( GarbageCollector *_gc, StringUnifier *unifier, const String &searchPath )
+: Collectable( _gc )
+, m_unifier( unifier )
 , m_nr( 0 )
 {
-  m_gc.reset( new GarbageCollector() );
   m_root = new Frame( getGC() );
+  setSearchPath( searchPath );
+  m_root->push_back( String::Static( "@needed" ), Value( new Array< CountPtr< Node >, 1 >( getGC() ) ) );
 }
 
 Context::~Context( void )
 {}
+
+void Context::gc_clear( void )
+{
+  m_root.reset();
+  m_unifier.reset();
+  m_searchPaths.clear();
+}
+
+void Context::gc_getChildren( Children &children ) const
+{
+  children.add( m_root );
+//  children.add( m_unifier );
+}
+
+Context &Context::setSearchPath( const String &searchPath )
+{
+  m_searchPaths.clear();
+
+  std::string s;
+  for( const char *c = searchPath.c_str(); (*c) != '\0'; ++c )
+  {
+    if( (*c) == ':' )
+    {
+      if( !s.empty() )
+      {
+        m_searchPaths.push_back( String( s ) );
+        s.clear();
+      }
+    }
+    else
+    {
+      s += (*c);
+    }
+  }
+
+  if( !s.empty() )
+  {
+    m_searchPaths.push_back( String( s ) );
+  }
+
+  return (*this);
+}
+
+const std::vector< String > &Context::getSearchPaths( void ) const
+{
+  return m_searchPaths;
+}
 
 CountPtr< Scope > Context::createScope( void )
 {
@@ -44,6 +95,7 @@ CountPtr< Graph > Context::createGraph( void ) const
 {
   CountPtr< Graph > graph( new Graph( getGC() ) );
 
+  /*
   // Just add every Node
   std::vector< const Frame* > stack;
   stack.push_back( m_root );
@@ -72,6 +124,39 @@ CountPtr< Graph > Context::createGraph( void ) const
         default: {}
       }
     }
+  }
+  */
+
+  const Value *const needed_v = m_root->getVariable( "@needed" );
+  if( !needed_v ) throw Exception() << "Looking up '@needed' failed";
+
+  if( !needed_v->isArray() )
+  {
+    throw Exception() << "'@needed' is not an Array, is " << needed_v->getTypeName();
+  }
+
+  ArrayBase *const needed_base = needed_v->getArray();
+  if( !needed_base )
+  {
+    throw Exception() << "Found Array '@needed', but is null";
+  }
+
+  typedef Array< CountPtr< Node >, 1 > NodeArray1D;
+  NodeArray1D *needed_p = 0;
+  if( !needed_base->getAs( needed_p ) )
+  {
+    throw Exception()
+      << "'@needed' is an Array, but not of the correct type, is "
+      << needed_base->getTypeName()
+      << needed_base->getSize()
+      ;
+  }
+
+  for( NodeArray1D::const_iterator n( needed_p->begin() ), end( needed_p->end() ); n != end; ++n )
+  {
+    Node *const node = n->get();
+    if( !node ) continue;
+    graph->addNode( node );
   }
 
   return graph;
--- libRPGML/RPGML/Context.h
+++ libRPGML/RPGML/Context.h
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -19,8 +19,10 @@
 #define RPGML_Context_h
 
 #include "GarbageCollector.h"
+#include "String.h"
 
 #include <memory>
+#include <vector>
 
 namespace RPGML {
 
@@ -30,28 +32,34 @@ class Frame;
 class StringUnifier;
 
 //! Do not allocate on the stack
-class Context : public Refcounted
+class Context : public Collectable
 {
 public:
+  typedef Collectable Base;
   explicit
-  Context( StringUnifier *unifier );
+  Context( GarbageCollector *_gc, StringUnifier *unifier, const String &searchPath = String::Static( "." ) );
 
   virtual ~Context( void );
+
+  Context &setSearchPath( const String &searchPath );
+  const std::vector< String > &getSearchPaths( void ) const;
 
   Frame *getRoot( void ) const { return m_root; }
 
   CountPtr< Scope > createScope( void );
 
   StringUnifier *getUnifier( void ) const { return m_unifier; }
-  GarbageCollector *getGC( void ) const { return m_gc.get(); }
 
   CountPtr< Graph > createGraph( void ) const;
 
   size_t getNr( void );
+
+  virtual void gc_clear( void );
+  virtual void gc_getChildren( Children &children ) const;
 private:
-  std::auto_ptr< GarbageCollector > m_gc;
   CountPtr< Frame > m_root;
   CountPtr< StringUnifier > m_unifier;
+  std::vector< String > m_searchPaths;
   size_t m_nr;
 };
 
