7f81a0163ebe8909be27292856147ecd68806e33
gunnar <gunnar.payer@web.de>
2015-01-06 23:34:34 +0100

    More replacements on the template Check whether plugin already exisits

--- ROOT/new_Node_plugin.sh
+++ ROOT/new_Node_plugin.sh
@@ -6,8 +6,80 @@ if [ $# -lt 1 ]; then
 fi 
 
 PluginName=$1
+PluginHeader="RPGML_Node_${PluginName}.h"
+PluginCPP="RPGML_Node_${PluginName}.cpp"
+
+if [ -e "$PluginHeader" -o -e "$PluginCPP" ]; then
+  echo "Plugin '$PluginName' already exists"
+  exit 4
+fi
+
+function get_namespaces()
+{
+  destdir=$(pwd)
+
+  namespaces=""
+  currdir=$(basename "$(pwd)")
+  while [ -n "$currdir" -a "$currdir" != "ROOT" ]; do
+    # reverse order
+    namespaces="${namespaces} ${currdir}"
+    cd ..
+    currdir=$(basename "$(pwd)")
+  done
+
+  if [ "$currdir" != "ROOT" ]; then
+    echo "Script must be called within RPGML's ROOT directory"
+    exit 3
+  fi
+
+  ROOT_dir="$(pwd)"
+  cd "$destdir"
+}
+
+function filter() # in, out
+{
+  if [ "$#" != "2" ]; then
+    echo "function filter() requires 2 arguments"
+    exit 2
+  fi
+
+  in="$1"
+  out="$2"
+
+  cat "$in" \
+     | sed \
+       -e "s/Node_TEMPLATE_h/Node_""$INCLUDE_GUARD_NAMESPACES""$PluginName""_h/g" \
+       -e "s/TEMPLATE_NAME/$PLUGIN_NAMESPACES_NAME$PluginName/g" \
+       -e "s/TEMPLATE/$PluginName/g" \
+       -e "s/EXTRA_NAMESPACES/$EXTRA_NAMESPACES/g" \
+       -e "s/ADDITIONAL_NAMESPACES_BEGIN/$ADDITIONAL_NAMESPACES_BEGIN/g" \
+       -e "s/ADDITIONAL_NAMESPACES_END/$ADDITIONAL_NAMESPACES_END \/\/ $ADDITIONAL_NAMESPACES_BEGIN/g" \
+    > "$out"
+}
+
+get_namespaces
+
+INCLUDE_GUARD_NAMESPACES=""
+EXTRA_NAMESPACES=""
+ADDITIONAL_NAMESPACES_BEGIN=""
+ADDITIONAL_NAMESPACES_END=""
+PLUGIN_NAMESPACES_NAME=""
+for n in $namespaces; do
+  EXTRA_NAMESPACES="$n::$EXTRA_NAMESPACES"
+  INCLUDE_GUARD_NAMESPACES="$n""_""$INCLUDE_GUARD_NAMESPACES"
+  ADDITIONAL_NAMESPACES_BEGIN="namespace ${n} { $ADDITIONAL_NAMESPACES_BEGIN"
+  ADDITIONAL_NAMESPACES_END="$ADDITIONAL_NAMESPACES_END }"
+  PLUGIN_NAMESPACES_NAME="$n"".""$PLUGIN_NAMESPACES_NAME"
+done
+
+echo EXTRA_NAMESPACES=${EXTRA_NAMESPACES}
+echo ADDITIONAL_NAMESPACES_BEGIN="'"${ADDITIONAL_NAMESPACES_BEGIN}"'"
+echo ADDITIONAL_NAMESPACES_END="'"${ADDITIONAL_NAMESPACES_END}"'"
+echo ROOT_dir="'"${ROOT_dir}"'"
+echo INCLUDE_GUARD_NAMESPACES="'"${INCLUDE_GUARD_NAMESPACES}"'"
+echo PLUGIN_NAMESPACES_NAME="'"${PLUGIN_NAMESPACES_NAME}"'"
 
 # TODO: Use a better path than ".."
-cat "../templates/RPGML_Node_TEMPLATE.h" | sed "s/TEMPLATE/$PluginName/g" > "RPGML_Node_$PluginName.h"
-cat "../templates/RPGML_Node_TEMPLATE.cpp" | sed "s/TEMPLATE/$PluginName/g" > "RPGML_Node_$PluginName.cpp"
+filter "${ROOT_dir}/../templates/RPGML_Node_TEMPLATE.h" "$PluginHeader"
+filter "${ROOT_dir}/../templates/RPGML_Node_TEMPLATE.cpp" "$PluginCPP"
 
