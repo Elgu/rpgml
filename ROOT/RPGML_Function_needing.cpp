68ee855d66918f6e606eabe17c93715cea97dd2b
gunnar <gunnar.payer@web.de>
2015-01-06 23:11:45 +0100

    Function to add Nodes to "@needed"

---
+++ ROOT/RPGML_Function_needing.cpp
@@ -0,0 +1,121 @@
+/* This file is part of RPGML.
+ *
+ * Copyright (c) 2014, Gunnar Payer, All rights reserved.
+ *
+ * RPGML is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+#include "RPGML_Function_needing.h"
+
+// RPGML_CXXFLAGS=
+// RPGML_LDFLAGS=
+
+#include <RPGML/String.h>
+#include <RPGML/Scope.h>
+#include <RPGML/Node.h>
+#include <RPGML/ParseException.h>
+
+using namespace std;
+
+namespace RPGML {
+
+
+Function_needing::Function_needing( GarbageCollector *_gc, Frame *parent, const SharedObject *so )
+: Function( _gc, parent, genDeclArgs(), so )
+{}
+
+Function_needing::~Function_needing( void )
+{}
+
+const char *Function_needing::getName( void ) const
+{
+  return "needing";
+}
+
+void Function_needing::gc_clear( void )
+{
+  Base::gc_clear();
+  m_needed.reset();
+}
+
+void Function_needing::gc_getChildren( Children &children ) const
+{
+  Base::gc_getChildren( children );
+  children.add( m_needed );
+}
+
+bool Function_needing::call_impl( const Location *loc, index_t recursion_depth, Scope *scope, Value &ret, index_t n_args, const Value *args )
+{
+  if( recursion_depth > MAX_RECURSION_DEPTH )
+  {
+    throw ParseException( loc ) << "Maximum recursion depth reached";
+  }
+
+  if( n_args != NUM_ARGS ) throw ParseException( loc ) << "Function needing requires " << NUM_ARGS << " arguments.";
+  const Value &in = args[ ARG_IN ];
+
+  if( in.isNode() )
+  {
+    if( m_needed.isNull() )
+    {
+      const Value *const needed_v = scope->lookup( "@needed" );
+      if( !needed_v ) throw ParseException( loc ) << "Looking up '@needed' failed";
+
+      if( !needed_v->isArray() )
+      {
+        throw ParseException( loc ) << "'@needed' is not an Array, is " << needed_v->getTypeName();
+      }
+
+      ArrayBase *const needed_base = needed_v->getArray();
+      if( !needed_base )
+      {
+        throw ParseException( loc ) << "Found Array '@needed', but is null";
+      }
+
+      NodeArray1D *needed_p = 0;
+      if( !needed_base->getAs( needed_p ) )
+      {
+        throw ParseException( loc )
+          << "'@needed' is an Array, but not of the correct type, is "
+          << needed_base->getTypeName()
+          << needed_base->getSize()
+          ;
+      }
+
+      m_needed.reset( needed_p );
+    }
+
+    m_needed->push_back( in.getNode() );
+  }
+  else
+  {
+    throw ParseException( loc )
+      << "Argument 'in' must be Node, is " << in.getTypeName()
+      ;
+  }
+
+  ret = Value( true );
+  return true;
+}
+
+CountPtr< Function::Args > Function_needing::genDeclArgs( void )
+{
+  CountPtr< Args > args = new Args( NUM_ARGS );
+  args->at( ARG_IN ) = Arg( String::Static( "in" ) );
+  return args;
+}
+
+ //
+} // namespace RPGML
+
+RPGML_CREATE_FUNCTION( needing,  )
---
+++ ROOT/RPGML_Function_needing.h
@@ -0,0 +1,57 @@
+/* This file is part of RPGML.
+ *
+ * Copyright (c) 2014, Gunnar Payer, All rights reserved.
+ *
+ * RPGML is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.
+ */
+#ifndef RPGML_Function_needing_h
+#define RPGML_Function_needing_h
+
+#include <RPGML/Function.h>
+#include <RPGML/Array.h>
+
+namespace RPGML {
+
+class Function_needing : public Function
+{
+public:
+  typedef Function Base;
+  EXCEPTION_BASE( Exception );
+
+  Function_needing( GarbageCollector *gc, Frame *parent, const SharedObject *so=0 );
+  virtual ~Function_needing( void );
+
+  virtual bool call_impl( const Location *loc, index_t recursion_depth, Scope *scope, Value &ret, index_t n_args, const Value *args );
+  virtual const char *getName( void ) const;
+
+  virtual void gc_clear( void );
+  virtual void gc_getChildren( Children &children ) const;
+private:
+  enum ArgNr
+  {
+    ARG_IN ,
+    NUM_ARGS
+  };
+
+  static
+  CountPtr< Args > genDeclArgs( void );
+
+  typedef Array< CountPtr< Node >, 1 > NodeArray1D;
+  CountPtr< NodeArray1D > m_needed;
+};
+
+} // namespace RPGML
+
+#endif
+
