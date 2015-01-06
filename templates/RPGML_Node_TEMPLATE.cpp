39a28513407214bfa365d1ae555539ce3f386260
gunnar <gunnar.payer@web.de>
2015-01-07 00:14:23 +0100

    Added garbage collector methods

--- templates/RPGML_Node_TEMPLATE.cpp
+++ templates/RPGML_Node_TEMPLATE.cpp
@@ -1,27 +1,31 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
 #include "RPGML_Node_TEMPLATE.h"
+
+// RPGML_CXXFLAGS=
+// RPGML_LDFLAGS=
 
 #include <algorithm>
 
 using namespace std;
 
 namespace RPGML {
+ADDITIONAL_NAMESPACES_BEGIN
 
 TEMPLATE::TEMPLATE( GarbageCollector *_gc, const String &identifier, const RPGML::SharedObject *so )
 : Node( _gc, identifier, so, NUM_INPUTS, NUM_OUTPUTS, NUM_PARAMS )
@@ -36,14 +40,24 @@ TEMPLATE::~TEMPLATE( void )
 
 const char *TEMPLATE::getName( void ) const
 {
-  return "TEMPLATE";
+  return "TEMPLATE_NAME";
+}
+
+void TEMPLATE::gc_clear( void )
+{
+  Base::gc_clear();
+}
+
+void TEMPLATE::gc_getChildren( Children &children ) const
+{
+  Base::gc_getChildren( children );
 }
 
 void TEMPLATE::set_foo( const Value &value, index_t )
 {
   if( value.getType() != Type::String() )
   {
-    throw Exception( "Param 'foo' must be set with string." );
+    throw Exception() << "Param 'foo' must be set with string.";
   }
 
   m_foo = value.getString();
@@ -51,20 +65,14 @@ void TEMPLATE::set_foo( const Value &value, index_t )
 
 bool TEMPLATE::tick( void )
 {
+  if( !hasAnyInputChanged() ) return true;
+  setAllOutputChanged();
+
   return true;
 }
 
-void TEMPLATE::gc_clear( void )
-{
-  Node::gc_clear();
-}
-
-void TEMPLATE::gc_getChildren( Children &children ) const
-{
-  Node::gc_getChildren( children );
-}
-
+ADDITIONAL_NAMESPACES_END
 } // namespace RPGML
 
-RPGML_CREATE_NODE( TEMPLATE )
+RPGML_CREATE_NODE( TEMPLATE, EXTRA_NAMESPACES )
 
--- templates/RPGML_Node_TEMPLATE.h
+++ templates/RPGML_Node_TEMPLATE.h
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -21,10 +21,12 @@
 #include <RPGML/Node.h>
 
 namespace RPGML {
+ADDITIONAL_NAMESPACES_BEGIN
 
 class TEMPLATE : public Node
 {
 public:
+  typedef Node Base;
   EXCEPTION_BASE( Exception );
 
   TEMPLATE( GarbageCollector *gc, const String &identifier, const SharedObject *so );
@@ -63,6 +65,7 @@ private:
   String m_foo;
 };
 
+ADDITIONAL_NAMESPACES_END
 } // namespace RPGML
 
 #endif
