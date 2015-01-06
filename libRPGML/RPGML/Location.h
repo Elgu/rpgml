b94cda20e67f5b4de86f105ca7112c6886ffdc89
gunnar <gunnar.payer@web.de>
2015-01-06 23:56:52 +0100

    Moved code into cpp file

--- libRPGML/RPGML/Location.h
+++ libRPGML/RPGML/Location.h
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -46,69 +46,17 @@ public:
     int column;
   };
 
-//  Location( void )
-//  {}
-
   explicit
   Location(
       const String &_filename
     , int begin_line=0, int begin_column=0
     , int end_line=0, int end_column=0
     , const Location *_parent=0
-    )
-  : filename( _filename )
-  , begin( begin_line, begin_column )
-  , end( end_line, end_column )
-  , parent( _parent )
-  {}
+    );
 
-  virtual ~Location( void )
-  {}
+  virtual ~Location( void );
 
-
-  std::ostream &print( std::ostream &o, bool with_filename=true, bool is_parent=false ) const
-  {
-    if( parent )
-    {
-      parent->print( o, with_filename, true );
-      o << std::endl;
-    }
-
-    if( is_parent ) o << "at ";
-
-    if( filename )
-    {
-      o << filename << ":";
-    }
-    if( begin.line > 0 )
-    {
-      o << begin.line;
-      if( begin.column > 0 )
-      {
-        o << "." << begin.column;
-      }
-      if( end.line >= begin.line )
-      {
-        o << "-";
-        if( end.line > begin.line )
-        {
-          o << end.line;
-        }
-        if(
-            end.column > 0
-          &&
-            (
-               ( end.line == begin.line && end.column > begin.column )
-            || ( end.line > begin.line )
-            )
-          )
-        {
-          o << "." << end.column;
-        }
-      }
-    }
-    return o;
-  }
+  std::ostream &print( std::ostream &o, bool with_filename=true, bool is_parent=false ) const;
 
   class WithoutFilename
   {
