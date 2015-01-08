3666c3431da029d5ce119dd52372f5a84ad70187
gunnar <gunnar.payer@web.de>
2015-01-08 22:22:28 +0100

    Using JobQueue::Job::work() doit() is now protected

--- libRPGML/utest/utest_Graph.cpp
+++ libRPGML/utest/utest_Graph.cpp
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -124,6 +124,7 @@ public:
     virtual ~EndWorker( void )
     {}
 
+  protected:
     virtual size_t doit( CountPtr< JobQueue > queue )
     {
       return size_t( -1 );
@@ -151,7 +152,7 @@ public:
       for(;;)
       {
         CountPtr< JobQueue::Job > job = q->getJob();
-        if( size_t(-1) == job->doit( q ) ) return 0;
+        if( size_t(-1) == job->work( q ) ) return 0;
         Thread::yield();
       }
     }
