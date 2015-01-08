04959768a3c7f477d4acf42845ca1da3359e948e
gunnar <gunnar.payer@web.de>
2015-01-08 22:07:29 +0100

    Added constructors taking CountPtr<> on StringData, so StringData does not get accidentaly deleted when constructing from that Added String::find(), String::mid() and String::npos String::m_c_str is now never NULL, also in String::Static() String::getData() now returns a CountPtr<>, so it can temporary create a StringData object for String::Static() strings

--- libRPGML/RPGML/String.cpp
+++ libRPGML/RPGML/String.cpp
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -36,6 +36,8 @@ namespace RPGML {
     return CountPtr< const StringData >( ret.get() );
   }
 
+  const size_t String::npos = size_t(-1);
+
   String::String( void )
   : m_c_str( empty_c_str )
   {}
@@ -43,11 +45,27 @@ namespace RPGML {
   String::String( const StringData *str )
   : m_str( str )
   , m_c_str( str ? str->get() : empty_c_str )
-  {}
+  {
+    if( !m_c_str ) m_c_str = empty_c_str;
+  }
+
+  String::String( const CountPtr< StringData > &str )
+  : m_str( str )
+  , m_c_str( str ? str->get() : empty_c_str )
+  {
+    if( !m_c_str ) m_c_str = empty_c_str;
+  }
+
+  String::String( const CountPtr< const StringData > &str )
+  : m_str( str )
+  , m_c_str( str ? str->get() : empty_c_str )
+  {
+    if( !m_c_str ) m_c_str = empty_c_str;
+  }
 
   String::String( const String &str )
-  : m_str( str.getData() )
-  , m_c_str( str.get() ) // str can be from Static(), so m_str would be 0
+  : m_str( str.m_str )
+  , m_c_str( str.m_c_str )
   {
     if( !m_c_str ) m_c_str = empty_c_str;
   }
@@ -81,7 +99,7 @@ namespace RPGML {
   String String::Static( const char *str )
   {
     String ret;
-    ret.m_c_str = str; // might be NULL
+    ret.m_c_str = ( str ? str : empty_c_str );
     return ret;
   }
 
@@ -130,16 +148,17 @@ namespace RPGML {
     return (*this);
   }
 
-  const StringData *String::getData( void ) const
+  CountPtr< const StringData > String::getData( void ) const
   {
-    if(
-         m_str.isNull()
-      && m_c_str != empty_c_str // static string literal used in this module
-      )
+    if( !m_str.isNull() )
     {
-      m_str.reset( new StdString( m_c_str ) );
+      return m_str;
     }
-    return m_str;
+    else
+    {
+      assert( m_c_str );
+      return new StdString( m_c_str );
+    }
   }
 
   String &String::moveFrom( std::string &str )
@@ -160,18 +179,19 @@ namespace RPGML {
 
   int String::compare( const char *other ) const
   {
-    if( !m_c_str )
+    assert( m_c_str );
+    if( m_c_str == other )
     {
-      if( !other ) return 0;
-      else return -1;
+      return 0;
     }
     else if( !other )
     {
-      return 1;
+      // NULL string is the same as an empty string
+      return ( empty() ? 0 : 1 );
     }
     else
     {
-      return strcmp( m_c_str, other );
+      return ::strcmp( m_c_str, other );
     }
   }
 
@@ -200,12 +220,33 @@ namespace RPGML {
   size_t String::length( void ) const
   {
     const char *const s = get();
-    return ( s ? strlen( s ) : 0 );
+    return ( s ? ::strlen( s ) : 0 );
   }
 
   size_t String::size( void ) const
   {
     return length();
+  }
+
+  size_t String::find( char c, size_t start_pos ) const
+  {
+    const size_t len = length();
+    const char *const s = c_str();
+    for( size_t pos = start_pos; pos < len; ++pos )
+    {
+      if( s[ pos ] == c ) return pos;
+    }
+    return npos;
+  }
+
+  String String::mid( size_t first_pos, size_t last_pos ) const
+  {
+    if( last_pos < first_pos ) return String();
+    const size_t len = length();
+    if( first_pos >= len ) return String();
+    if( last_pos >= len ) last_pos = len-1;
+    const char *const s = c_str();
+    return String( s+first_pos, last_pos - first_pos + 1 );
   }
 
   index_t String::count( void ) const
@@ -226,7 +267,11 @@ namespace RPGML {
   bool String::operator==( const char *other ) const { return compare( other ) == 0; }
   bool String::operator!=( const char *other ) const { return compare( other ) != 0; }
 
-  char String::operator[] ( size_t i ) const { return m_str->get()[ i ]; }
+  char String::operator[] ( size_t i ) const
+  {
+    assert( m_c_str ); // m_c_str is always valid
+    return m_c_str[ i ];
+  }
 
   String &String::operator+=( const String &other )
   {
@@ -248,5 +293,6 @@ namespace RPGML {
     this->swap( tmp );
     return (*this);
   }
+
 } // namespace RPGML
 
--- libRPGML/RPGML/String.h
+++ libRPGML/RPGML/String.h
@@ -1,17 +1,17 @@
 /* This file is part of RPGML.
- * 
+ *
  * Copyright (c) 2014, Gunnar Payer, All rights reserved.
- * 
+ *
  * RPGML is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.
  */
@@ -86,10 +86,18 @@ private:
 class String
 {
 public:
+  static const size_t npos;
+
   String( void );
 
   //! Adds ref to str
   String( const StringData *str );
+
+  //! Adds ref to str
+  String( const CountPtr< StringData > &str );
+
+  //! Adds ref to str
+  String( const CountPtr< const StringData > &str );
 
   //! Adds ref to str
   String( const String &str );
@@ -141,7 +149,7 @@ public:
     return get();
   }
 
-  const StringData *getData( void ) const;
+  CountPtr< const StringData > getData( void ) const;
 
   String &moveFrom( std::string &str );
 
@@ -155,6 +163,9 @@ public:
 
   size_t length( void ) const;
   size_t size( void ) const;
+
+  size_t find( char c, size_t start_pos = 0 ) const;
+  String mid( size_t first_pos, size_t last_pos = npos ) const;
 
   index_t count( void ) const;
 
@@ -178,7 +189,7 @@ public:
   String &operator+=( const char *other );
 
 private:
-  mutable CountPtr< const StringData > m_str;
+  CountPtr< const StringData > m_str;
   const char *m_c_str;
   static const char *const empty_c_str;
 };
