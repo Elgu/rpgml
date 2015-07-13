6b5a20b4fa9105793138361420bbd8f10344a4dc
gunnar <gunnar.payer@web.de>
2015-07-13 21:52:57 +0200

    Support for arbitrary map offset

--- ROOT/util/RPGML_util.h
+++ ROOT/util/RPGML_util.h
@@ -20,6 +20,7 @@
 
 #include <RPGML/GarbageCollector.h>
 #include <RPGML/ArrayData.h>
+#include <RPGML/types.h>
 
 #include <sys/mman.h>
 #include <unistd.h>
@@ -55,11 +56,12 @@ public:
 
   void clear( void )
   {
-    if( m_addr != nullptr )
+    if( m_map_addr != nullptr )
     {
-      ::munmap( m_addr, m_map_length );
-      m_addr = nullptr;
+      ::munmap( m_map_addr, m_map_length );
+      m_map_addr = nullptr;
       m_map_length = 0;
+      m_addr = nullptr;
     }
 
     if( -1 != m_fd )
@@ -88,17 +90,12 @@ private:
 
     const size_t page_size = ::sysconf( _SC_PAGE_SIZE );
 
-    if( 0 != ( offset_bytes % page_size ) )
-    {
-      throw Exception()
-        << "'offset_bytes' must be a multiple of the page size"
-        << ": is " << offset_bytes
-        << ", page size is " << page_size
-        ;
-    }
+    const size_t map_offset_bytes = offset_bytes - offset_bytes % page_size;
+    offset_bytes -= map_offset_bytes;
 
-    const size_t length_elements_bytes = length_elements*sizeof(Element);
-    const size_t map_length = ( length_elements_bytes + (page_size-1) ) % page_size;
+    const size_t length_elements_bytes = offset_bytes + length_elements*sizeof(Element);
+    size_t map_length = ( length_elements_bytes + (page_size-1) );
+    map_length -= map_length % page_size;
 
     try
     {
@@ -111,19 +108,33 @@ private:
           ;
       }
 
-      m_addr = reinterpret_cast< Element* >(
-        ::mmap( nullptr, map_length, prot, MAP_SHARED, m_fd, offset_bytes )
+      m_map_addr = reinterpret_cast< uint8_t* >(
+        ::mmap( nullptr, map_length, prot, MAP_SHARED, m_fd, map_offset_bytes )
         );
-      if( !m_addr )
+      if( !m_map_addr )
       {
         throw Exception()
           << "Could not map already opened file '" << filename << "'"
           << ": " << strerror( errno )
           ;
       }
+      m_addr = reinterpret_cast< Element* >( m_map_addr + offset_bytes );
 
       Base::setElements( m_addr, length_elements );
       m_map_length = map_length;
+
+      std::cerr
+        << "map()" << std::endl
+        << ": length_elements = " << length_elements << std::endl
+        << ", page_size = " << page_size << std::endl
+        << ", map_offset_bytes = " << map_offset_bytes << std::endl
+        << ", offset_bytes = " << offset_bytes << std::endl
+        << ", length_elements_bytes = " << length_elements_bytes << std::endl
+        << ", map_length = " << map_length << std::endl
+        << ", m_map_addr = " << size_t( m_map_addr ) << std::endl
+        << ", m_addr = " << size_t( m_addr ) << std::endl
+        << ", m_map_length = " << m_map_length << std::endl
+        ;
     }
     catch( ... )
     {
@@ -133,6 +144,7 @@ private:
   }
 
   Element *m_addr = nullptr;
+  uint8_t *m_map_addr = nullptr;
   size_t m_map_length = 0;
   int m_fd = -1;
 };
